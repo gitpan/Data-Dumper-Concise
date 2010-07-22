eea186040a5fc7f816ffa3931968923dc187ae5a
Arthur Axel "fREW" Schmidt <frioux+cpan@gmail.com>
2010-07-22 04:49:16 +0000

    Import of FREW/Data-Dumper-Concise-2.000 from CPAN.
    gitpan-cpan-distribution: Data-Dumper-Concise
    gitpan-cpan-version:      2.000
    gitpan-cpan-path:         FREW/Data-Dumper-Concise-2.000.tar.gz
    gitpan-cpan-author:       FREW
    gitpan-cpan-maturity:     released

--- lib/Data/Dumper/Concise/Sugar.pm
+++ lib/Data/Dumper/Concise/Sugar.pm
@@ -4,14 +4,22 @@ use 5.006;
 
 use Exporter ();
 use Data::Dumper::Concise ();
+use Devel::ArgNames;
 
 BEGIN { @ISA = qw(Exporter) }
 
-@EXPORT = qw(Dwarn DwarnS);
+@EXPORT = qw(Dwarn DwarnS DwarnL DwarnN);
 
-sub Dwarn { warn Data::Dumper::Concise::Dumper @_; @_ }
+sub Dwarn { return DwarnL(@_) if wantarray; DwarnS($_[0]) }
+
+sub DwarnL { warn Data::Dumper::Concise::Dumper @_; @_ }
 
 sub DwarnS ($) { warn Data::Dumper::Concise::Dumper $_[0]; $_[0] }
+
+sub DwarnN ($) {
+   my $x = arg_names();
+   warn(($x?$x:'(anon)') . ' => ' . Data::Dumper::Concise::Dumper $_[0]); $_[0]
+}
 
 =head1 NAME
 
@@ -27,9 +35,15 @@ is equivalent to:
 
   use Data::Dumper::Concise;
 
-  my @return = some_call(...);
-  warn Dumper(@return);
-  return @return;
+  if (wantarray) {
+     my @return = some_call(...);
+     warn Dumper(@return);
+     return @return;
+  } else {
+     my $return = some_call(...);
+     warn Dumper($return);
+     return $return;
+  }
 
 but shorter. If you need to force scalar context on the value,
 
@@ -45,8 +59,65 @@ is equivalent to:
   warn Dumper($return);
   return $return;
 
-Another trick that is extremely useful when doing method chaining is the
-following:
+If you need to force list context on the value,
+
+  use Data::Dumper::Concise::Sugar;
+
+  return DwarnL some_call(...)
+
+is equivalent to:
+
+  use Data::Dumper::Concise;
+
+  my @return = some_call(...);
+  warn Dumper(@return);
+  return @return;
+
+=head1 DESCRIPTION
+
+  use Data::Dumper::Concise::Sugar;
+
+will import Dwarn, DwarnL, and DwarnS into your namespace. Using L<Exporter>, so see
+its docs for ways to make it do something else.
+
+=head2 Dwarn
+
+  sub Dwarn { return DwarnL(@_) if wantarray; DwarnS($_[0]) }
+
+=head2 DwarnL
+
+  sub Dwarn { warn Data::Dumper::Concise::Dumper @_; @_ }
+
+=head2 DwarnS
+
+  sub DwarnS ($) { warn Data::Dumper::Concise::Dumper $_[0]; $_[0] }
+
+=head1 TIPS AND TRICKS
+
+=head2 global usage
+
+Instead of always just doing:
+
+  use Data::Dumper::Concise::Sugar;
+
+  Dwarn ...
+
+We tend to do:
+
+  perl -MData::Dumper::Concise::Sugar foo.pl
+
+(and then in the perl code:)
+
+  ::Dwarn ...
+
+That way, if you leave them in and run without the
+C<< use Data::Dumper::Concise::Sugar >> the program will fail to compile and
+you are less likely to check it in by accident.  Furthmore it allows that
+much less friction to add debug messages.
+
+=head2 method chaining
+
+One trick which is useful when doing method chaining is the following:
 
   my $foo = Bar->new;
   $foo->bar->baz->Data::Dumper::Concise::Sugar::DwarnS->biff;
@@ -55,21 +126,6 @@ which is the same as:
 
   my $foo = Bar->new;
   (DwarnS $foo->bar->baz)->biff;
-
-=head1 DESCRIPTION
-
-  use Data::Dumper::Concise::Sugar;
-
-will import Dwarn and DwarnS into your namespace. Using L<Exporter>, so see
-its docs for ways to make it do something else.
-
-=head2 Dwarn
-
-  sub Dwarn { warn Data::Dumper::Concise::Dumper @_; @_ }
-
-=head3 DwarnS
-
-  sub DwarnS ($) { warn Data::Dumper::Concise::Dumper $_[0]; $_[0] }
 
 =head1 SEE ALSO
 
--- t/sugar.t
+++ t/sugar.t
@@ -14,11 +14,38 @@ BEGIN {
    }
 }
 
-my @foo = Dwarn 'warn', 'friend';
-is $warned_string,qq{"warn"\n"friend"\n}, 'Dwarn warns';
+DWARNL: {
+   my @foo = DwarnL 'warn', 'friend';
+   is $warned_string,qq{"warn"\n"friend"\n}, 'DwarnL warns';
 
-ok eq_array(\@foo, ['warn','friend']), 'Dwarn passes through correctly';
+   ok eq_array(\@foo, ['warn','friend']), 'DwarnL passes through correctly';
+}
 
-my $bar = DwarnS 'robot',2,3;
-is $warned_string,qq{"robot"\n}, 'DwarnS warns';
-is $bar, 'robot', 'DwarnS passes through correctly';
+DWARNS: {
+   my $bar = DwarnS 'robot',2,3;
+   is $warned_string,qq{"robot"\n}, 'DwarnS warns';
+   is $bar, 'robot', 'DwarnS passes through correctly';
+}
+
+DWARN: {
+   my @foo = Dwarn 'warn', 'friend';
+   is $warned_string,qq{"warn"\n"friend"\n}, 'Dwarn warns lists';
+
+   ok eq_array(\@foo, ['warn','friend']), 'Dwarn passes lists through correctly';
+
+   my $bar = Dwarn 'robot',2,3;
+   is $warned_string,qq{"robot"\n}, 'Dwarn warns scalars correctly';
+   is $bar, 'robot', 'Dwarn passes scalars through correctly';
+}
+
+DWARNN: {
+   my $x = [1];
+   my $foo = DwarnN $x;
+   is $warned_string, qq{\$x => [\n  1\n]\n}, 'DwarnN warns';
+
+   ok eq_array($foo, [1]), 'DwarnN passes through correctly';
+
+   DwarnN [1];
+   is $warned_string, qq{(anon) => [\n  1\n]\n}, 'DwarnN warns';
+}
+
